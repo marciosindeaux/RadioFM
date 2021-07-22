9f189fe0b79a54e8a226560fbb8a4c5aefa8cae5
Marcio Alexandre <sindeaux1999@hotmail.com>
2021-07-22 01:54:22 -0300

    feat: criacao do enum de atividades do bot

---
+++ src/app/enums/ActivityEnum.js
@@ -0,0 +1,10 @@
+class ActivityEnum {
+    static values() {
+        return {
+            PLAYING_A_GAME:0,
+            LIVE_ON_TWITCH:1,
+            LISTENING_TO:2,
+            WATCHING:3
+        }
+    }
+}
