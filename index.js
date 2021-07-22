9c913c34872048360feb49319050b68d463b2b17
Marcio Alexandre <sindeaux1999@hotmail.com>
2021-07-22 01:56:33 -0300

    feat: criacao das acoes iniciais no index.js

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
-require('dotenv').config({path:'./src/environments/.env'})
+require('dotenv').config({path:'./src/envs/.env'})
 const Discord = require('discord.js');
+const ytdl = require('ytdl-core')
+const ClientEvents = Discord.Constants.Events;
 const client = new Discord.Client();
 
-console.log(process.env)
+
+const StartEvent = require('./src/app/events/StartEvent')
+const RegexUtils = require('./src/app/utils/RegexUtils')
+const RegexEnum = require ('./src/app/enums/RegexEnum')
+
+console.log(process.env.Auth_Token)
+client.login(process.env.Auth_Token);
+
+
+client.on(ClientEvents.CLIENT_READY , () => {
+    console.log(`Started At ${new Date()}`);
+    StartEvent.doEvent(client)
+    RegexUtils.isRegexCompativelComTexto(
+        "!!play https://www.youtube.com/watch?v=WZIGwN-5Ioo in <#861605749226209310>", 
+        RegexEnum.values().youtube.pattern
+    )
+});
+
+
+const streamOptions = {seek:0, volume:1};
+
+client.on(ClientEvents.MESSAGE_CREATE , (message) => {
+    if(!message.author.bot){
+        if(message.content.indexOf("<#") !== -1) {
+            let voice = message.guild.channels.resolve('861605749226209310')
+            voice.join().then( connection => {
+                const stream = ytdl('https://www.youtube.com/watch?v=WZIGwN-5Ioo', { quality:'highestaudio' ,filter: 'audioonly'})
+                const playMusic = connection.play(stream);
+                playMusic.on('end', () => {
+                    voice.leave();
+                })
+            })
+        }
+    }
+})
